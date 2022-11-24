b550b7187dfc2c30d0b4464c45a747f5fdad597e
kashubian <karalusmichal@gmail.com>
2022-11-24 19:52:01 +0100

    Add router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Recipes from 'components/Recipes'
+
 function App() {
-  return <div></div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Recipes />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
