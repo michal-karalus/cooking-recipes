e0141567c34d33ddb0b3cc1b83fe904009c525d9
kashubian <karalusmichal@gmail.com>
2022-12-05 11:19:07 +0100

    Add header component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Recipes from 'components/Recipes'
 import Search from 'components/Search'
+import Header from './Header'
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/recipes" element={<Recipes />} />
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+import styles from './Header.module.scss'
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__wrapper}>
+        <Link className={styles.logo} to={'/'}>
+          <h1>Recipes</h1>
+        </Link>
+        <nav className={styles.navbar}>
+          <Link className={styles.link} to={'/'}>
+            Home
+          </Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
