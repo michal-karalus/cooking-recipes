b89c26f64c0249312e2c89314f47a0d83c2d2767
kashubian <karalusmichal@gmail.com>
2022-12-06 23:47:21 +0100

    Add side menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Recipes from 'components/Recipes'
 import Search from 'components/Search'
 import Header from 'components/Header'
+import SideMenu from 'components/SideMenu'
 
 function App() {
   return (
     <Router>
       <Header />
+      <SideMenu />
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/recipes" element={<Recipes />} />
---
+++ src/components/SideMenu/index.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import classnames from 'classnames'
+
+import styles from './SideMenu.module.scss'
+
+function SideMenu() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen((current) => !current)
+  }
+
+  return (
+    <>
+      <button
+        className={classnames(
+          styles.hamburger,
+          isOpen ? styles.hamburgerActive : ''
+        )}
+        onClick={toggleMenu}
+      >
+        <div className={styles.hamburgerInner}>
+          <span className={styles.hamburgerBar}></span>
+        </div>
+      </button>
+      <nav className={classnames(styles.menu, isOpen ? styles.menuActive : '')}>
+        <div className={styles.menuWrapper}>
+          <Link className={styles.link} to={'/'} onClick={toggleMenu}>
+            Home
+          </Link>
+          <Link className={styles.link} to={'/'}>
+            Find recipes
+          </Link>
+          <Link className={styles.link} to={'/'}>
+            What's in your fridge
+          </Link>
+        </div>
+      </nav>
+      <div
+        className={classnames(
+          styles.backdrop,
+          isOpen ? styles.backdropActive : ''
+        )}
+      ></div>
+    </>
+  )
+}
+
+export default SideMenu
