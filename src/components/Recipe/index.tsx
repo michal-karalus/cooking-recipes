7ca59ec672ff1cb4dffb92fa26d7c1613d5b0e8f
kashubian <karalusmichal@gmail.com>
2022-12-12 22:06:18 +0100

    Add recipe details components

--- src/components/Recipe/index.tsx
+++ src/components/Recipe/index.tsx
@@ -1,18 +1,57 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { fetchRecipe } from 'api'
 
+import styles from './Recipe.module.scss'
+
+interface IRecipeDetails {
+  id: string
+  title: string
+  image: string
+  analyzedInstructions: any
+  extendedIngredients: any
+}
+
 function Recipe() {
   const { id } = useParams()
 
+  const [recipe, setRecipe] = useState<IRecipeDetails>()
+
   useEffect(() => {
     fetchRecipe(id!).then((response) => {
-      console.log(response.data)
+      setRecipe(response.data)
     })
-  })
+  }, [id])
 
-  return <div>{id}</div>
+  return (
+    <article>
+      {recipe && (
+        <div className="container">
+          <h2 className={styles.title}>{recipe.title}</h2>
+          <img className={styles.photo} src={recipe.image} alt="" />
+          <div className="row">
+            <div className={styles.ingredients}>
+              <h3>Ingredients</h3>
+              <ul>
+                {recipe.extendedIngredients.map((item: any) => {
+                  return <li key={item.id}>{item.original}</li>
+                })}
+              </ul>
+            </div>
+            <div className={styles.steps}>
+              <h3>Steps</h3>
+              <ol>
+                {recipe.analyzedInstructions[0].steps.map((step: any) => {
+                  return <li key={step.number}>{step.step}</li>
+                })}
+              </ol>
+            </div>
+          </div>
+        </div>
+      )}
+    </article>
+  )
 }
 
 export default Recipe
