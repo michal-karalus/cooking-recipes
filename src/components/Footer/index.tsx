edb593e4923f3dbe1c8623641271a4a596c6ff6a
kashubian <karalusmichal@gmail.com>
2023-01-05 15:23:43 +0100

    Add nbsp for footer link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,16 +4,16 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
-        <span>Application is using </span>
+        <span>Application is using&nbsp;</span>
         <a
           className={styles.link}
           href="https://spoonacular.com/food-api/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Spoonacular API
+          Spoonacular API&nbsp;
         </a>
-        <span> to provide recipes data.</span>
+        <span>to provide recipes data.</span>
       </div>
     </footer>
   )
