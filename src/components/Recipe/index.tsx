10793de8596d8f9861038b1e4fbd4b670cd4eb60
kashubian <karalusmichal@gmail.com>
2023-02-01 18:58:08 +0100

    Fetch recipe details data with react query

--- src/components/Recipe/index.tsx
+++ src/components/Recipe/index.tsx
@@ -1,37 +1,36 @@
-import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
 
 import { fetchRecipe } from 'api'
 import Loader from 'components/common/Loader'
-import { Ingredient, RecipeDetails, Step } from 'types'
+import { Ingredient, Step } from 'types'
 
 import styles from './Recipe.module.scss'
 
 function Recipe() {
   const { id } = useParams()
 
-  const [recipe, setRecipe] = useState<RecipeDetails>()
-  const [isLoading, setIsLoading] = useState<boolean>(true)
-
-  useEffect(() => {
-    fetchRecipe(id!).then((response) => {
-      setRecipe(response.data)
-      setIsLoading(false)
-    })
-  }, [id])
+  const { data, isLoading } = useQuery(
+    'recipe',
+    () => fetchRecipe(id!).then((response) => response.data),
+    {
+      cacheTime: 0,
+      staleTime: 0,
+    }
+  )
 
   return (
     <>
       {isLoading && <Loader />}
-      {recipe && (
+      {data && (
         <div className={styles.container}>
-          <h2 className={styles.title}>{recipe.title}</h2>
-          <img className={styles.photo} src={recipe.image} alt={recipe.title} />
+          <h2 className={styles.title}>{data.title}</h2>
+          <img className={styles.photo} src={data.image} alt={data.title} />
           <div className={styles.details}>
             <div className={styles.ingredients}>
               <h3 className={styles.ingredients__title}>Ingredients</h3>
               <ul className={styles.ingredients__list}>
-                {recipe.extendedIngredients.map((ingredient: Ingredient) => {
+                {data.extendedIngredients.map((ingredient: Ingredient) => {
                   return (
                     <li
                       className={styles.ingredients__item}
@@ -46,7 +45,7 @@ function Recipe() {
             <div className={styles.steps}>
               <h3 className={styles.steps__title}>Steps</h3>
               <ol className={styles.steps__list}>
-                {recipe.analyzedInstructions[0].steps.map((step: Step) => {
+                {data.analyzedInstructions[0].steps.map((step: Step) => {
                   return (
                     <li className={styles.steps__item} key={step.number}>
                       {step.step}
