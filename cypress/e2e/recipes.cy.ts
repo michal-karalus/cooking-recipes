eb5365ae5b81e566a72f4003de58dbcb6b85a3e1
kashubian <karalusmichal@gmail.com>
2023-02-20 19:41:47 +0100

    Add e2e tests for recipes list

--- cypress/e2e/recipes.cy.ts
+++ cypress/e2e/recipes.cy.ts
@@ -1,5 +1,29 @@
 describe('Recipes', () => {
-  it('should pass', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+
+  it('should display list of recipes', () => {
+    cy.intercept(`${Cypress.env('API_URL')}/recipes/complexSearch*`, {
+      fixture: 'recipesList.json',
+    })
+    cy.findByLabelText(/name/i).type('banana')
+    cy.findByText(/search/i).click()
+    cy.location('pathname').should('eq', '/recipes')
+    cy.findAllByTestId('recipe').should('have.length', 3)
+  })
+
+  it('should display empty list and be able to go back to main page', () => {
+    cy.intercept(`${Cypress.env('API_URL')}/recipes/complexSearch*`, {
+      fixture: 'recipesEmptyList.json',
+    })
+    cy.findByLabelText(/name/i).type('non existing recipe')
+    cy.findByText(/search/i).click()
+    cy.findByTestId('recipeNotFound').should(
+      'have.text',
+      "We couldn't find recipes for provided criteria."
+    )
+    cy.findByText(/back to home/i).click()
+    cy.location('pathname').should('eq', '/')
   })
 })
