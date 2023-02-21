15385cb880ad0510d2871d3a7c074a1d717834ef
kashubian <karalusmichal@gmail.com>
2023-02-21 16:55:05 +0100

    Add reporter for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   videoUploadOnPasses: false,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
   },
 })
