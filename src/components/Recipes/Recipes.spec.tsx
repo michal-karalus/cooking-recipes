68858fcf142282e99dd1ef980f0bfc16ba7ca77e
kashubian <karalusmichal@gmail.com>
2023-02-28 21:31:03 +0100

    Add mock data and refactor recipes test

--- src/components/Recipes/Recipes.spec.tsx
+++ src/components/Recipes/Recipes.spec.tsx
@@ -2,32 +2,16 @@ import { render, screen } from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { fetchRecipes } from 'api'
+import { recipes } from 'mockData'
 
 import Recipes from '.'
 
 jest.mock('api')
 
-const recipes = {
-  data: {
-    results: [
-      {
-        id: 634006,
-        title: 'Banana Bread',
-        image: 'https://spoonacular.com/recipeImages/634006-312x231.jpg',
-        imageType: 'jpg',
-      },
-      {
-        id: 634171,
-        title: 'Banana Pudding Cake',
-        image: 'https://spoonacular.com/recipeImages/634171-312x231.jpg',
-        imageType: 'jpg',
-      },
-    ],
-  },
-}
+const mockedFetchRecipes = fetchRecipes as jest.Mock<any>
 
 beforeEach(() => {
-  ;(fetchRecipes as jest.Mock).mockResolvedValue(recipes)
+  mockedFetchRecipes.mockResolvedValue(recipes)
 })
 
 it('should render list of recipes', async () => {
